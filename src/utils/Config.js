d7b72df3226e40a194d3919aa6ae4e4ddc1d60ba
Luiz <luizpierinovitor@gmail.com>
2024-07-25 13:16:05 -0300

    Mensagem

--- src/utils/Config.js
+++ src/utils/Config.js
@@ -1,68 +1,37 @@
 class Config {
-    //BASE_URL = "https://medicalstoreapi.herokuapp.com/";
-  //FOR LIVE URL
-  // static loginUrl = "https://medicalstoreapi.herokuapp.com/api/gettoken/";
-  // static refreshApiUrl =
-  //   "https://medicalstoreapi.herokuapp.com/api/resfresh_token/";
-  // static companyApiUrl = "https://medicalstoreapi.herokuapp.com/api/company/";
-  // static homeApiUrl = "https://medicalstoreapi.herokuapp.com/api/home_api/";
-  // static customerRequestApiUrl =
-  //   "https://medicalstoreapi.herokuapp.com/api/customer_request/";
-  // static medicineNameApiUrl =
-  //   "https://medicalstoreapi.herokuapp.com/api/medicinebyname/";
-  // static companyBankApiUrl =
-  //   "https://medicalstoreapi.herokuapp.com/api/companybank/";
-  // static generateBillApiUrl =
-  //   "https://medicalstoreapi.herokuapp.com/api/generate_bill_api/";
-  // static companyAccountApiUrl =
-  //   "https://medicalstoreapi.herokuapp.com/api/companyaccount/";
-  // static companyOnly = "https://medicalstoreapi.herokuapp.com/api/companyonly/";
-  // static employeeApiURL = "https://medicalstoreapi.herokuapp.com/api/employee/";
-  // static medicineApiUrl = "https://medicalstoreapi.herokuapp.com/api/medicine/";
-  // static employeeBankApiUrl =
-  //   "https://medicalstoreapi.herokuapp.com/api/employee_all_bank/";
-  // static employeeBankApiUrlBYID =
-  //   "https://medicalstoreapi.herokuapp.com/api/employee_bankby_id/";
-  // static employeeSalaryApiUrl =
-  //   "https://medicalstoreapi.herokuapp.com/api/employee_all_salary/";
-  // static employeeSalaryByIdApiUrl =
-  //   "https://medicalstoreapi.herokuapp.com/api/employee_salaryby_id/";
-  static loginUrl = "http://127.0.0.1:8000/api/gettoken/";
-  static refreshApiUrl = "http://127.0.0.1:8000/api/resfresh_token/";
-  static companyApiUrl = "http://127.0.0.1:8000/api/company/";
-  static homeApiUrl = "http://127.0.0.1:8000/api/home_api/";
-  static customerRequestApiUrl = "http://127.0.0.1:8000/api/customer_request/";
-  static medicineNameApiUrl = "http://127.0.0.1:8000/api/medicinebyname/";
-  static companyBankApiUrl = "http://127.0.0.1:8000/api/companybank/";
-  static generateBillApiUrl = "http://127.0.0.1:8000/api/generate_bill_api/";
-  static companyAccountApiUrl = "http://127.0.0.1:8000/api/companyaccount/";
-  static companyOnly = "http://127.0.0.1:8000/api/companyonly/";
-  static employeeApiURL = "http://127.0.0.1:8000/api/employee/";
-  static medicineApiUrl = "http://127.0.0.1:8000/api/medicine/";
-  static employeeBankApiUrl = "http://127.0.0.1:8000/api/employee_all_bank/";
+  static apiUrl = "https://djangobackend-wha5.vercel.app";
+  static loginUrl = this.apiUrl + "/api/gettoken/";
+  static refreshApiUrl = this.apiUrl + "/api/resfresh_token/";
+  static companyApiUrl = this.apiUrl + "/api/company/";
+  static homeApiUrl = this.apiUrl + "/api/home_api/";
+  static customerRequestApiUrl = this.apiUrl + "/api/customer_request/";
+  static medicineNameApiUrl = this.apiUrl + "/api/medicinebyname/";
+  static companyBankApiUrl = this.apiUrl + "/api/companybank/";
+  static generateBillApiUrl = this.apiUrl + "/api/generate_bill_api/";
+  static companyAccountApiUrl = this.apiUrl + "/api/companyaccount/";
+  static companyOnly = this.apiUrl + "/api/companyonly/";
+  static employeeApiURL = this.apiUrl + "/api/employee/";
+  static medicineApiUrl = this.apiUrl + "/api/medicine/";
+  static employeeBankApiUrl = this.apiUrl + "/api/employee_all_bank/";
   static employeeBankApiUrlBYID =
-    "http://127.0.0.1:8000/api/employee_bankby_id/";
+    this.apiUrl + "/api/employee_bankby_id/";
   static employeeSalaryApiUrl =
-    "http://127.0.0.1:8000/api/employee_all_salary/";
+    this.apiUrl + "/api/employee_all_salary/";
   static employeeSalaryByIdApiUrl =
-    "http://127.0.0.1:8000/api/employee_salaryby_id/";
+    this.apiUrl + "/api/employee_salaryby_id/";
   static logoutPageUrl = "/logout";
   static homeUrl = "/home";
 
-    
-
-    
-
-    static sidebarItem = [
-        { index: "0", title: "Inicio", url: "/home", icons: "home" },
-        { index: "1", title: "Empresa Farmacêutica", url: "/company", icons: "assessment" },
-        { index: "2", title: "Adicionar Medicamento", url: "/addMedicine", icons: "assessment" },
-        { index: "3", title: "Gerenciar Medicamento", url: "/manageMedicine", icons: "assessment" },
-        { index: "4", title: "Gerenciar Conta da Empresa", url: "/manageCompanyAccount", icons: "assessment" },
-        { index: "5", title: "Gerenciar Funcionário", url: "/employeeManage", icons: "assessment" },
-        { index: "6", title: "Gerar Conta", url: "/generateBill", icons: "assessment" },
-        { index: "7", title: "Solicitação dos Clientes", url: "/customerRequest", icons: "assessment" },
-    ];
+  static sidebarItem = [
+    { index: "0", title: "Inicio", url: "/home", icons: "home" },
+    { index: "1", title: "Empresa Farmacêutica", url: "/company", icons: "assessment" },
+    { index: "2", title: "Adicionar Medicamento", url: "/addMedicine", icons: "assessment" },
+    { index: "3", title: "Gerenciar Medicamento", url: "/manageMedicine", icons: "assessment" },
+    { index: "4", title: "Gerenciar Conta da Empresa", url: "/manageCompanyAccount", icons: "assessment" },
+    { index: "5", title: "Gerenciar Funcionário", url: "/employeeManage", icons: "assessment" },
+    { index: "6", title: "Gerar Conta", url: "/generateBill", icons: "assessment" },
+    { index: "7", title: "Solicitação dos Clientes", url: "/customerRequest", icons: "assessment" },
+  ];
 }
 
 export default Config;
